181b7e7ff2df93de6041a21c7e3934a680415969
mdalvinsifat <mdalvinsifat@gmail.com>
2026-04-16 17:51:35 +0600

    store-name-change

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './Redux/store.js'
+import { store } from './Redux/Store.js'
 
 
 createRoot(document.getElementById('root')).render(
